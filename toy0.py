3ea024de33f7dcc9d34a516d387bb59387a2c8b3
tualatint <tualatint@gmail.com>
2019-09-06 00:04:23 +0200

    put excution code into main

--- toy0.py
+++ toy0.py
@@ -4,14 +4,17 @@ import time
 # env = gym.make('MountainCar-v0')
 env = gym.make("CartPole-v0")
 env.reset()
+step = 0
 for _ in range(100):
     env.render()
     action = env.action_space.sample()
-    print("action:", action)
+    # print("action:", action)
     observation, reward, done, info = env.step(action)  # take a random action
-    print("s,r,d,i", observation, reward, done, info)
+    # print("s,r,d,i", observation, reward, done, info)
+    step += 1
     if done:
-        print("Reset....")
+        print("Reset....", step)
         env.reset()
+        step = 0
     # time.sleep(0.03)
 env.close()
--- toy_mc_q.py
+++ toy_mc_q.py
@@ -81,82 +81,85 @@ def choose_action(Q, s):
 
 params = {"batch_size": 640, "shuffle": True, "num_workers": 4}
 max_epoch = 3000
-for loop in range(1):
-    ####### collect data #########
+if __name__ == "__main__":
+    for loop in range(1):
+        ####### collect data #########
 
-    print("##########  Loop :", loop)
-    s = np.zeros((4, 1))
-    step = 0
-    record = []
-    record_list = []
-    episode = 0
-    t_step = np.int64(0)
-    total_r = 0
-    act_1 = np.int64(0)
-    last_observation = np.zeros((4))
-    max_step = np.int64(1e6)
-    for i in range(max_step):
-        # env.render()
-        if loop == 0:
-            action = env.action_space.sample()  # random action
-        else:
-            if step == 0:
-                action = env.action_space.sample()
-                last_observation = np.zeros((4))
+        print("##########  Loop :", loop)
+        s = np.zeros((4, 1))
+        step = 0
+        record = []
+        record_list = []
+        episode = 0
+        t_step = np.int64(0)
+        total_r = 0
+        act_1 = np.int64(0)
+        last_observation = np.zeros((4))
+        max_step = np.int64(1e6)
+        for i in range(max_step):
+            # env.render()
+            if loop == 0:
+                action = env.action_space.sample()  # random action
             else:
-                if np.random.rand() < 1.0 / (loop + 3):
+                if step == 0:
                     action = env.action_space.sample()
+                    last_observation = np.zeros((4))
                 else:
-                    action = choose_action(Q, s)
-        step += 1
-        act_1 += action
-        observation, reward, done, info = env.step(action)
-        s = observation
-        total_r += reward
-        record.append(
-            [last_observation, observation, action, reward, total_r, not done]
-        )
-        last_observation = observation.copy()
-        if done:
-            t_step += step
-            episode += 1
-            step = 0
-            record_list.append(record.copy())
-            record.clear()
-            env.reset()
-            total_r = 0
-    score = t_step / episode
-    print("average steps:{:.4f}".format(t_step / episode))
-    print("average action:{:.4f}".format(act_1 / max_step))
-    env.close()
-    S = Sampler(record_list)
-    training_generator = data.DataLoader(S, **params)
+                    if np.random.rand() < 1.0 / (loop + 3):
+                        action = env.action_space.sample()
+                    else:
+                        action = choose_action(Q, s)
+            step += 1
+            act_1 += action
+            observation, reward, done, info = env.step(action)
+            s = observation
+            total_r += reward
+            record.append(
+                [last_observation, observation, action, reward, total_r, not done]
+            )
+            last_observation = observation.copy()
+            if done:
+                t_step += step
+                episode += 1
+                step = 0
+                record_list.append(record.copy())
+                record.clear()
+                env.reset()
+                total_r = 0
+        score = t_step / episode
+        print("average steps:{:.4f}".format(t_step / episode))
+        print("average action:{:.4f}".format(act_1 / max_step))
+        env.close()
+        S = Sampler(record_list)
+        training_generator = data.DataLoader(S, **params)
 
-    ####### train Q network #########
-    try:
-        for epoch in range(max_epoch):
-            total_loss = 0.0
-            iter = 0
-            for xr, y in training_generator:
-                iter += 1
-                y = y.view(-1, 1).to(device, dtype=torch.float)
-                x = np.concatenate(
-                    (np.array(xr[0]), np.array(xr[2]).reshape(-1, 1)), axis=1
-                )
-                out = Q.forward(torch.Tensor(x).to(device, dtype=torch.float)).view(
-                    -1, 1
-                )
-                loss = criterion(y, out)
-                total_loss += loss
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-            print("Epoch {}: {:.4f}".format(epoch, total_loss / iter))
-    except (KeyboardInterrupt, SystemExit):
-        model_file_name = "q_net_v1_" + str(len(record_list)) + "_" + str(loop) + ".pth"
-        torch.save(Q.state_dict(), "./models/" + model_file_name)
-        print("Model file: " + model_file_name + " saved.")
+        ####### train Q network #########
+        try:
+            for epoch in range(max_epoch):
+                total_loss = 0.0
+                iter = 0
+                for xr, y in training_generator:
+                    iter += 1
+                    y = y.view(-1, 1).to(device, dtype=torch.float)
+                    x = np.concatenate(
+                        (np.array(xr[0]), np.array(xr[2]).reshape(-1, 1)), axis=1
+                    )
+                    out = Q.forward(torch.Tensor(x).to(device, dtype=torch.float)).view(
+                        -1, 1
+                    )
+                    loss = criterion(y, out)
+                    total_loss += loss
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                print("Epoch {}: {:.4f}".format(epoch, total_loss / iter))
+        except (KeyboardInterrupt, SystemExit):
+            model_file_name = (
+                "q_net_v1_" + str(len(record_list)) + "_" + str(loop) + ".pth"
+            )
+            torch.save(Q.state_dict(), "./models/" + model_file_name)
+            print("Model file: " + model_file_name + " saved.")
 
-model_file_name = "q_net_v1_" + str(len(record_list)) + "_" + str(loop) + ".pth"
-torch.save(Q.state_dict(), "./models/" + model_file_name)
-print("Model file: " + model_file_name + " saved.")
+    model_file_name = "q_net_v1_" + str(len(record_list)) + "_" + str(loop) + ".pth"
+    torch.save(Q.state_dict(), "./models/" + model_file_name)
+    print("Model file: " + model_file_name + " saved.")
